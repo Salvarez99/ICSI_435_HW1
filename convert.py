2ee7b29aca70edd4e6a443327ce124470f78473c
Salvarez99 <Stephenalvarez3@gmail.com>
2023-09-05 20:46:47 -0400

    files modified

--- convert.py
+++ convert.py
@@ -1,49 +1,46 @@
 import numpy as np
 
+
 class convert:
-    letters = np.array(["S", "A", "B", "C", "D", 'E', 'F', 'H', 'P', 'Q', 'R', 'G'])
-    vertices = {'S' : 0,
-                'A' : 1,
-                'B' : 2,
-                'C' : 3,
-                'D' : 4,
-                'E' : 5,
-                'F' : 6,
-                'H' : 7,
-                'P' : 8,
-                'Q' : 9,
-                'R' : 10,
-                'G' : 11}
+    letters = np.array(["S", "A", "B", "C", "D", 'E','F', 'H', 'P', 'Q', 'R', 'G'])
+    vertices = {'S': 0,
+                'A': 1,
+                'B': 2,
+                'C': 3,
+                'D': 4,
+                'E': 5,
+                'F': 6,
+                'H': 7,
+                'P': 8,
+                'Q': 9,
+                'R': 10,
+                'G': 11}
+
     matrix: np.ndarray
     vertex_list: dict[str, list[str]]
 
-    def __init__(self,matrix: np.ndarray, vertex_list: dict[str, list[str]]):
-
-        self.matrix = matrix
-        self.vertex_list = vertex_list
-
-        return 
-
-    def convertListToMatrixUW(self):
-        for key, values in self.vertex_list.items():    
+    @classmethod
+    def convertListToMatrixUW(self, matrix: np.ndarray, vertex_list: dict[str, list[str]]):
+        for key, values in vertex_list.items():
             if key in self.vertices:
                 row = self.vertices.get(key)
                 for value in values:
                     col = self.vertices.get(value)
-                    self.matrix[row][col] = 1
+                    matrix[row][col] = 1
 
-        return self.matrix
+        return matrix
 
-    def printAdjMatrix(matrix: np.ndarray, vertices : np.ndarray):
+    @classmethod
+    def printAdjMatrix(self, matrix: np.ndarray):
         print("   ", end=" ")
-        for i in range(vertices.size):
-            print(f"{vertices[i]}", end=" ")
-    
+        for i in range(self.letters.size):
+            print(f"{self.letters[i]}", end=" ")
+
         print("\n")
         for rows in range(12):
-            print(f"{vertices[rows]}: ", end=" ")
+            print(f"{self.letters[rows]}: ", end=" ")
             for cols in range(12):
                 print(f"{matrix[rows,cols]}", end=" ")
             print("\n")
-    
+
         return
--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-import numpy as np 
-import sys
-sys.path.append('/Users/xenep/OneDrive/Documents/UAlbany/Fall 2023/ICSI 435 AI/Alvarez_S_Homework1/convert.py')
 from convert import *
+import numpy as np
+import sys
+sys.path.append(
+    '/Users/xenep/OneDrive/Documents/UAlbany/Fall 2023/ICSI 435 AI/Alvarez_S_Homework1/convert.py')
 
-matrix_1 = np.zeros((12,12), dtype=int)
-matrix_2 = np.zeros((12,12), dtype=int)
+matrix_1 = np.zeros((12, 12), dtype=int)
+matrix_2 = np.zeros((12, 12), dtype=int)
 
-graph_1 = {'S' : ['D','E','P'],       #^
-           'A' : ['B','C'],           #^ 
-           'B' : ['A','D'],           #^ 
-           'C' : ['A','D','F'],       #^      
-           'D' : ['B','C','E','S'],   #^
-           'E' : ['D','H','R','S'],   #^
-           'F' : ['C','G','R'],       #^
-           'H' : ['E','P','Q'],       #^
-           'P' : ['H','Q','S'],       #^
-           'Q' : ['H','P'],           #^
-           'R' : ['E','F'],           #^
-           'G' : ['F']}               #^
+graph_1 = {'S': ['D', 'E', 'P'],
+           'A': ['B', 'C'],
+           'B': ['A', 'D'],
+           'C': ['A', 'D', 'F'],
+           'D': ['B', 'C', 'E', 'S'],
+           'E': ['D', 'H', 'R', 'S'],
+           'F': ['C', 'G', 'R'],
+           'H': ['E', 'P', 'Q'],
+           'P': ['H', 'Q', 'S'],
+           'Q': ['H', 'P'],
+           'R': ['E', 'F'],
+           'G': ['F']}
 
-graph_2 = {'S' : ['D','E','P'],       #^ 
-           'B' : ['A'],               #^ 
-           'C' : ['A'],               #^      
-           'D' : ['B','C','E'],       #^
-           'E' : ['H','R'],           #^
-           'F' : ['C','G'],           #^
-           'H' : ['P','Q'],           #^
-           'P' : ['Q'],               #^
-           'R' : ['F']}               #^
+graph_2 = {'S': ['D', 'E', 'P'],
+           'B': ['A'],
+           'C': ['A'],
+           'D': ['B', 'C', 'E'],
+           'E': ['H', 'R'],
+           'F': ['C', 'G'],
+           'H': ['P', 'Q'],
+           'P': ['Q'],
+           'R': ['F']}
 
-test = convert(matrix_1, graph_1)
-test_2 = convert(matrix_2, graph_2)
+matrix_1 = convert.convertListToMatrixUW(matrix_1, graph_1)
+matrix_2 = convert.convertListToMatrixUW(matrix_2, graph_2)
 
+convert.printAdjMatrix(matrix_1)
+print()
+convert.printAdjMatrix(matrix_2)
 
-
-matrix = test.convertListToMatrixUW();
-matrix_2 = test_2.convertListToMatrixUW();
-
-print(matrix_2)
