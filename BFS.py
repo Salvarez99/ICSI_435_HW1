9c8afe488092ec970f3a8627d1135ef35e757585
Salvarez99 <Stephenalvarez3@gmail.com>
2023-09-08 22:57:34 -0400

    file modified

--- BFS.py
+++ BFS.py
@@ -4,41 +4,21 @@ import numpy as np
 class BFS:
 
     @classmethod
-    def BFS_stack_recursive(vertex_list: dict[str, list[str]]):
+    def BFS_queue_recursive(self, vertex_list: dict[str, list[str]]):
 
         return
 
     @classmethod
-    def BFS_stack_iterative(vertex_list: dict[str, list[str]]):
+    def BFS_queue_iterative(self, vertex_list: dict[str, list[str]]):
 
         return
 
     @classmethod
-    def BFS_queue_recursive(vertex_list: dict[str, list[str]]):
+    def BFS_queue_recursive(self, adj_matrix: np.ndarray):
 
         return
 
     @classmethod
-    def BFS_queue_iterative(vertex_list: dict[str, list[str]]):
-
-        return
-
-    @classmethod
-    def BFS_stack_recursive(adj_matrix: np.ndarray):
-
-        return
-
-    @classmethod
-    def BFS_stack_iterative(adj_matrix: np.ndarray):
-
-        return
-
-    @classmethod
-    def BFS_queue_recursive(adj_matrix: np.ndarray):
-
-        return
-
-    @classmethod
-    def BFS_queue_iterative(adj_matrix: np.ndarray):
+    def BFS_queue_iterative(self, adj_matrix: np.ndarray):
 
         return
